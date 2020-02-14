3e07ce358ef2df73c4d32d0288689c113bb553ec
cmgospod <cmgospod@users.noreply.github.com>
2020-02-14 15:42:31 -0600

    Basic class structure

--- src/character.py
+++ src/character.py
@@ -1,1 +1,15 @@
 # adventuring hero
+class Character():
+    def __init__(self, name, level, class, major_trait=None, minor_trait=None):
+        self.name = name
+        self.level = level
+        self.class = class
+        self.major_trait = major_trait
+        self.minor_trait = minor_trait
+
+    def __str__(self):
+        return f'{self.name}, a level {self.level} {self.class}.'
+
+
+class Hero(Character):
+    def
--- src/location.py
+++ src/location.py
@@ -1,1 +1,26 @@
 # Location class
+
+class Location():
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x =  x
+        self.y = y
+    def __str__(self):
+        return f'{self.name} at {self.x}, {self.y}.'
+
+class Town(Location):
+    def __init__(self, name, x, y, population):
+        super().__init__(name, x, y)
+        self.population = population
+
+    def __str__(self):
+        super().__str__(self)
+
+
+class Dungeon(Location):
+    def __init__(self, name, x, y, difficulty):
+        super().__init__(name, x, y)
+        self.difficulty = difficulty
+
+    def __str__(self):
+        super().__str__(self)
